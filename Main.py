721d5bf013dba9657e210f6876f3ca95e8c52e18
WillMon <w.montero34@gmail.com>
2016-04-19 12:05:52 -0500

    1st Commit 4/19/016
    Stage out main, Trying to understand how to print to screen

---
+++ Main.py
@@ -0,0 +1,9 @@
+import pygame 
+from Node import *
+
+searchSpace = []
+for x in range(10):
+	for y in range(10):
+		n = Node(x,y)
+		
+		peramiter = True if(x>)
--- Nodes.py
+++ Nodes.py
@@ -1,15 +1,19 @@
-import sys, math
+import pygame as gfx 
 
-class Node:
-	x = 0
-	y = 0
-	current = None
-	List = []
-	
-	def __init__(self,x,y current)
+
+class Node(object):
+
+	def __init__(self, x = 0, y=0):
+
 	self.x = x
 	self.y = y
-	self.List(current)	
+	self.parent = 
+	None
+	self.G = None
+	self.H = None
+	self.F = self.G + self.H
+	
+	
 
 
 
