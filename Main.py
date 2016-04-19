57a7d9a81ab4d70e627ff4f3ceb406a12ef12379
WillMon <w.montero34@gmail.com>
2016-04-19 18:19:16 -0500

    3rd Commit
    Had poulated my Nodes Class and staged my Astar Class. Setted for information display. Also setted up the grid

--- Main.py
+++ Main.py
@@ -1,9 +1,28 @@
 import pygame 
-from Node import *
+from Nodes import *
 
-searchSpace = []
-for x in range(10):
-	for y in range(10):
-		n = Node(x,y)
+def main():
+	searchSpace = []
+	for x in range(10):
+		for y in range(10):
+			n = Node(x,y)
 		
-		peramiter = True if(x>)
+			
+			searchSpace.append(n)
+		
+	pygame.init()
+	WINDOW_SIZE = [255,255]
+	
+	screen = pygame.display.set_mode(WINDOW_SIZE)
+	
+	pygame.display.set_caption("ADGP_!20")
+	
+	currentTime = pygame.time.get_ticks()
+	print(currentTime)
+	
+main()
+
+
+
+
+
--- Nodes.py
+++ Nodes.py
@@ -1,18 +1,37 @@
-
-
-
 class Node(object):
 
 	def __init__(self, x = 0, y=0):
-
+		
 		self.x = x
 		self.y = y
 		self.parent = None
 		self.G = None
 		self.H = None
 		self.F = None
+		
+	def setF(self):
+		self.F = self.G + self.H
+	def getH(self):
+		tempNode = Node()
+	def getG(self, c_grid):
+		for n in c_grid:
+			
+		
+		
+		
+	#nl.sort(key lambda x: x: x.f)
+class AStar(object):
+
+	def __init__(self, nl):
+		self.NodeList = nl
+		self.currentNode = Node()
+	
+	def sort(self):
+		temp = []
+		placeHolder = 0
+		for n in self.NodeList:
+			if 
+			
+		
 	
 	
-
-
-
